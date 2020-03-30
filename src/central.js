604d5dea90b5524d60f4ba147eaf4468db52573d
Gavin Matthews <gmatthews@axway.com>
2020-03-30 09:35:43 +0100

    Pause

--- src/central.js
+++ src/central.js
@@ -1,43 +1,64 @@
-const access = require('./access');
-const config = require('./config');
+const access = require("./access");
+const config = require("./config");
+const walk = require("fs-walk");
+const yaml = require("js-yaml");
 
 // TODO: Should string together order dynamically from defs.
 const ORDER = [
-    "Gateway",
-    "Stage",
-    "Environment",
-    "APIService",
-    "APIServiceRevision",
-    "APIServiceInstance",
-    "VirtualAPI",
-    "VirtualAPIDefinition",
-    "PathRoute",
-    "Rules",
-    "Deployment"
+  "Gateway",
+  "Stage",
+  "Environment",
+  "APIService",
+  "APIServiceRevision",
+  "APIServiceInstance",
+  "VirtualAPI",
+  "VirtualAPIDefinition",
+  "PathRoute",
+  "Rules",
+  "Deployment"
 ];
 
 // Load the yaml and convert to resource
-async function process(deleted, modified) {
-    await access.getAccessToken();
-
-    // TODO ORDERING
-    deleted.forEach(deleteResource);
-
-    modified.sort((l,r) => ORDER.indexOf(l.kind) < ORDER.indexOf(r.kind))
-        .forEach(syncResource);
-} 
-
-async function deleteResource(deletedPath) {
-    console.log("DELETED: " + deletedPath);
+function loadYaml(yamlFile) {
+  const raw = fs.readFileSync(yamlFile, "utf8");
+  const doc = yaml.safeLoad(raw);
+  return doc;
 }
 
-async function syncResource(yaml) {
-    // TODO convert the yaml to a json.
-    // FORMAT the creation url
-    console.log(yaml);
-    console.log("-----------------------------------");
+// For simplicity going to load the entire project and diff it.
+// It's the easiest way to find deletions and multiple resources per file.
+function loadProject() {
+  const yamls = [];
+  return new Promise((resolve, reject) => {
+    console.log("fido", process.env.GITHUB_WORKSPACE)
+    walk.files(process.env.GITHUB_WORKSPACE, 
+      (basedir, filename, stat, next) => {
+        console.log(filename);
+        yamls.push(loadYaml(path.join(basedir, filename)));
+        next();
+      }, 
+      (err) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(yamls);
+      });
+    });
+}
+
+
+// Load the yaml and convert to resource
+async function processProject() {
+  //await access.getAccessToken();
+  try {
+    const yamls = await loadProject();
+    console.log(yamls);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
 
 module.exports = {
-    process
-}
+  process: processProject
+};
--- src/main.js
+++ src/main.js
@@ -1,46 +1,14 @@
-const yaml = require('js-yaml');
-const central = require('./central');
-const config = require('./config');
-const fs   = require('fs');
-const path = require('path');
-const core = require('@actions/core');
+const central = require("./central");
+const core = require("@actions/core");
 
 async function run() {
   try {
-    await processFiles();
+    await central.process();
     console.log("✅ Deploy Complete");
-  }
-  catch (error) {
+  } catch (error) {
     console.error("⚠️ Error deploying", error);
     core.setFailed(error.message);
   }
 }
 
 run();
-
-// Load the yaml and convert to resource
-function loadYaml(yamlFile) {
-  const yamlPath = path.join(
-      process.env.GITHUB_WORKSPACE,
-      yamlFile
-  );
-
-  const raw = fs.readFileSync(yamlPath, 'utf8');
-  const doc = yaml.safeLoad(raw);
-
-  return doc;
-} 
-
-/**
- * Process the files
- */
-async function processFiles() {
-  const files = config.files.split(' ').filter(n => !n.startsWith('.'));
-  console.log("FILES: ", files);
-
-  const deleted = files.filter(f => !fs.existsSync(f));
-  const modified = files.filter(f => !(deleted.indexOf(f)!==-1)).map(n => loadYaml(n))
-    .filter(d => d.kind && d.group && d.spec);
-
-  await central.process(deleted, modified);  
-}
